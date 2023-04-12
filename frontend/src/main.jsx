031e8ba384b02c87158532ed15afa3158b14993c
Nataphan-Rak <nataphandeark@gmail.com>
2023-04-12 21:35:31 +0700

    create-login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthProvider } from './context/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
