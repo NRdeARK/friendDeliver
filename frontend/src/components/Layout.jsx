f6dd1debbc0dbfbc6b2a38a2a5e1afdfab32ccbf
BaitoeyKP <kanyarat_toey@hotmail.com>
2023-04-24 15:17:32 +0700

    update

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import  { Outlet } from "react-router-dom" 
-import Navbar from './Navbar'
-import AboutUs from './AboutUs'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import AboutUs from "./AboutUs";
 function Layout() {
   return (
-    <main className='w-screen h-screen bg-amber-400'>
-        <Navbar/>
-        <Outlet/>
-        {/* <AboutUs/> */}
-
+    <main className="w-screen h-screen bg-amber-400">
+      <Navbar />
+      <Outlet />
+      {/* <AboutUs/> */}
     </main>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
