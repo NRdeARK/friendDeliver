f01b9be1ee16d26ae7b6e57edb9882bfbb423d2b
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2023-04-27 16:45:55 +0700

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -29,59 +29,48 @@ function Navbar() {
     <nav>
       <div className="shadow-md w-full fixed top-0 left-0 saturate-0 z-50	">
         <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
-          <span className="text-3xl text-indigo-600 mr-1 pt-2 "></span>
-          <ion-icon name="chevron-back"></ion-icon>
-          <ion-icon name="code"></ion-icon>
-          <ion-icon name="chevron-forward"></ion-icon>
-          <ion-icon name=""></ion-icon>
-          <p>    FriendDeliver</p>
-        </div>
-        <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
-        <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
-        </div>
-        <ul className= {`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-10 opacity-100' : 'top-[-400px]'} md:opacity-100 opacity-0 `}>
-          {
-            Links.map((link)=>(
-          // <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
-          //   <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
-          //   Designer
-          // </div>
-          // <div
-          //   onClick={() => setOpen(!open)}
-          //   className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-          // >
-          //   <ion-icon name={open ? "close" : "menu"}></ion-icon>
-          // </div>
-          // <ul
-          //   className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-          //     open ? "top-10 opacity-100" : "top-[-400px]"
-          //   } md:opacity-100 opacity-0 `}
-          // >
-          //   {auth.user == null
-          //   ?
-          //   LinksUnAuth.map((link) => (
-              <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-                <Link
-                  to={link.link}
-                  className="text-gray-800 hover:text-gray-400 duration-500"
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))
-            :
-            LinksAuth.map((link) => (
-              <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-                <Link
-                  to={link.link}
-                  className="text-gray-800 hover:text-gray-400 duration-500"
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))
-          }
+          <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
+            <span className="text-3xl text-indigo-600 mr-1 pt-2 "></span>
+            <ion-icon name="chevron-back"></ion-icon>
+            <ion-icon name="code"></ion-icon>
+            <ion-icon name="chevron-forward"></ion-icon>
+            <ion-icon name=""></ion-icon>
+            <p>    FriendDeliver</p>
+          </div>
+          <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
+            <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+          </div>
+          <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
+            Designer
+          </div>
+          <ul
+            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-10 opacity-100" : "top-[-400px]"
+              } md:opacity-100 opacity-0 `}
+          >
+            {auth.user == null
+              ?
+              LinksUnAuth.map((link) => (
+                <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                  <Link
+                    to={link.link}
+                    className="text-gray-800 hover:text-gray-400 duration-500"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))
+              :
+              LinksAuth.map((link) => (
+                <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                  <Link
+                    to={link.link}
+                    className="text-gray-800 hover:text-gray-400 duration-500"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </div>
