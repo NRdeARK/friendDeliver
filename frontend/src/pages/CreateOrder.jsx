7c97aff7ac9877989f42577db3c4a5e5a0241b6c
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2023-04-27 16:52:52 +0700

    update

--- frontend/src/pages/CreateOrder.jsx
+++ frontend/src/pages/CreateOrder.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 import CreateOrderForm from "../components/OrderCreateForm";
-import OrderBlog from "../components/OrderBlog";
+import { getAllOrder, createOrder } from '../api/OrderService'
+import OrderTicket from '../components/orderTicket'
 
-// import {useState, Component, useEffect} from 'react'
+import {useState, Component, useEffect} from 'react'
 // import VerifyTicket from '../components/verifyTicket'
 // const ORDER_URL = "/Order";
 // import axios from "../api/axios";
 
-function CreateOrder() {
-  return (
-    <div className="bg-amber-400 mb-[155px]">
-      <div className="ml-[400px] mt-[100px]">
-        <CreateOrderForm></CreateOrderForm>
-      </div>
-      <div className="flex items-center justify-center">
-        <OrderBlog></OrderBlog>
-      </div>
-    </div>
-  );
+
+class CreateOrder extends Component {
+
+
+    getAllUsers = () => {
+        getAllOrder()
+          .then(users => {
+            console.log(users)
+            this.setState({users: users, numberOfUsers: users.length})
+          });
+    }
+    
+   
+    render (){
+        return (
+          <div class='bg-amber-400 mb-[155px]'>
+            <div class='ml-[400px] mt-[100px]'>
+              <CreateOrderForm></CreateOrderForm>
+            </div>
+            <div class="  flex items-center justify-center">
+            <OrderTicket>  </OrderTicket>
+            </div>
+          </div>
+          
+          
+      
+        )
+    }
+// function CreateOrder() {
+//   return (
+//     <div className="bg-amber-400 mb-[155px]">
+//       <div className="ml-[400px] mt-[100px]">
+//         <CreateOrderForm></CreateOrderForm>
+//       </div>
+//       <div className="flex items-center justify-center">
+//         <OrderBlog></OrderBlog>
+//       </div>
+//     </div>
+//   );
 }
 
 // function CreateOrder() {
