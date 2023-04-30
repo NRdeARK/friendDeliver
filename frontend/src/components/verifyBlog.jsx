ba9e21cd1508dbbd0591fadf0d6527f60d35ef43
Nataphan-Rak <nataphandeark@gmail.com>
2023-04-30 16:30:41 +0700

    update orderstatus

--- frontend/src/components/verifyBlog.jsx
+++ frontend/src/components/verifyBlog.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 import VerifyTicket from "./verifyTicket";
-import OrderConfirm from "./OrderConfirmForm";
 import useAuth from "../hooks/useAuth";
 import { useEffect } from "react";
 import axios from "../api/axios";
 import OrderConfirmForm from "./OrderConfirmForm";
-import OrderReceiveForm from "./OrderReceiveForm";
 
 const ORDER_POSTID_URL = "Order/";
 
 function VerifyBlog(data) {
   const props = data.data;
-  console.log(props)
+  console.log(data);
   let content;
   const { auth } = useAuth();
   const [orderList, setOrderList] = useState([]);
@@ -28,27 +26,29 @@ function VerifyBlog(data) {
   }, []);
 
   if (auth.user == props.username) {
-    
-
     content = (
       <div>
         <VerifyTicket props={data} type={"Selective"}></VerifyTicket>
         {orderList.map((item) => {
-          if(props.username == item.props.username)
-            return <OrderConfirmForm type={"owner"} props={data}></OrderConfirmForm>;
+          if (props.username == item.username)
+            return (
+              <OrderConfirmForm type={"owner"} props={item}></OrderConfirmForm>
+            );
           else
-          return <OrderConfirmForm type={"other"} props={data}></OrderConfirmForm>;
-           
-          
+            return (
+              <OrderConfirmForm type={"other"} props={item}></OrderConfirmForm>
+            );
         })}
       </div>
     );
   } else {
     content = (
       <div>
-        <VerifyTicket props={data} type={"Other"} ></VerifyTicket>
-        {orderList.map((data) => {
-          return <OrderConfirmForm type={"other"} props={data}></OrderConfirmForm>;
+        <VerifyTicket props={data} type={"Other"}></VerifyTicket>
+        {orderList.map((item) => {
+          return (
+            <OrderConfirmForm type={"other"} props={item}></OrderConfirmForm>
+          );
           // if(data.username == auth.user){
           // // return <OrderReceiveForm props={data}></OrderReceiveForm>;
           // }
